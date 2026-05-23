d0c4a4b4e9c45c1fdead6257b9d9105e07885756
Titan <khapd.23it@vku.udn.vn>
2026-05-22 20:27:49 -0800

    feat: update hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,37 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const {
+  SEPOLIA_RPC_URL,
+  DEPLOYER_PRIVATE_KEY,
+  ETHERSCAN_API_KEY,
+} = process.env;
+
+const sepoliaAccounts = DEPLOYER_PRIVATE_KEY ? [DEPLOYER_PRIVATE_KEY] : [];
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: { enabled: true, runs: 200 },
+    },
+  },
+  networks: {
+    hardhat: {},
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
+    sepolia: {
+      url: SEPOLIA_RPC_URL || "https://ethereum-sepolia-rpc.publicnode.com",
+      accounts: sepoliaAccounts,
+      chainId: 11155111,
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY || "",
+  },
+  mocha: {
+    timeout: 60000,
+  },
 };
